b9d902cb0afa1d73dbde4d12b4e17b00a3be55a8
velasciraptor <amyvelasquez432@gmail.com>
2021-11-21 14:36:56 -0600

    legend

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -40,32 +40,7 @@ L.geoJSON(data, {
         return L.circleMarker(latlng);
       },
     // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // Create a legend control object.
-    let legend = L.control({
-        legend.onAdd = function() {
-            let div = L.DomUtil.create("div", "info legend");
-            const magnitudes = [0, 1, 2, 3, 4, 5];
-            const colors = [
-            "#98ee00",
-            "#d4ee00",
-            "#eecc00",
-            "#ee9c00",
-            "#ea822c",
-            "#ea2c2c"
-            ];
-            // Looping through our intervals to generate a label with a colored square for each interval.
-            for (var i = 0; i < magnitudes.length; i++) {
-                console.log(colors[i]);
-                div.innerHTML +=
-                "<i style='background: " + colors[i] + "'></i> " +
-                magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-            }
-            return div;
-            };
-            legend.addTo(map);
-        };
-    position: "bottomright"});
+  style: styleInfo,
     // We create a popup for each circleMarker to display the magnitude and
     //  location of the earthquake after the marker has been created and styled.
     onEachFeature: function(feature, layer) {
@@ -74,6 +49,30 @@ L.geoJSON(data, {
     // Then we add the earthquake layer to our map.
     earthquakes.addTo(map);
 });
+
+// Create a legend control object.
+let legend = L.control({position: "bottomright"});
+// Then add all the details for the legend.
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"];
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+        }
+    return div;
+};
+legend.addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on the map. 
 // We pass the magnitude of the earthquake into two separate functions to calculate the color and radius.
