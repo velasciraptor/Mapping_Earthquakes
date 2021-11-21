290575144e2e5132171feb3cde7107b4a2496b79
velasciraptor <amyvelasquez432@gmail.com>
2021-11-21 15:34:53 -0600

    tweaking code

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -47,33 +47,40 @@ L.geoJSON(data, {
     layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);}
 }).addTo(earthquakes);
     // Then we add the earthquake layer to our map.
-    earthquakes.addTo(map);
-});
+  earthquakes.addTo(map);
 
-// Create a legend control object.
-let legend = L.control({position: "bottomright"});
-// Then add all the details for the legend.
+  // Here we create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+});
+  
+  // Then add all the details for the legend
 legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
+  let div = L.DomUtil.create("div", "info legend");
+  
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
     "#98ee00",
     "#d4ee00",
     "#eecc00",
     "#ee9c00",
     "#ea822c",
-    "#ea2c2c"];
-    // Looping through our intervals to generate a label with a colored square for each interval.
-    for (var i = 0; i < magnitudes.length; i++) {
-        console.log(colors[i]);
-        div.innerHTML +=
-            "<i style='background: " + colors[i] + "'></i> " +
-            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-        }
+    "#ea2c2c"
+  ];
+  
+  // Looping through our intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
     return div;
-};
-legend.addTo(map);
+  };
 
+  // Finally, we our legend to the map.
+  legend.addTo(map);
+});
 // This function returns the style data for each of the earthquakes we plot on the map. 
 // We pass the magnitude of the earthquake into two separate functions to calculate the color and radius.
 function styleInfo(feature) {
